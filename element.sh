1a70baed59f720e5ae97a554e2e587564b14bd26
Sahil Butt <sahil17999@gmail.com>
2024-06-30 18:55:32 +0000

    feat: output messages for different elements complete

--- element.sh
+++ element.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
 
-if [[ $1 -eq 1 || $1 == "H" || $1 == "Hydrogen" ]]
-then
-  echo "The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
-else
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+ARG=$1
+
+#If the argument is empty:
+
+if [[ -z $ARG ]]
+then 
   echo "Please provide an element as an argument."
+  exit
 fi
+
+#Check if the argument is a number then it must be the atomic number:
+
+if [[ $1 =~ ^[1-9]+$ ]]
+then 
+  PRINT=$($PSQL " SELECT atomic_number, types.type, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name FROM properties INNER JOIN elements USING (atomic_number) INNER JOIN types USING (type_id) WHERE atomic_number = $1 ")
+#Otherwise if the argument is a string then it must be the atomic name or symbol:
+else
+  PRINT=$($PSQL " SELECT atomic_number, types.type, atomic_mass, melting_point_celsius, boiling_point_celsius, symbol, name FROM properties INNER JOIN elements USING (atomic_number) INNER JOIN types USING (type_id) WHERE name = '$1' OR symbol = '$1' ")
+fi
+
+#If the element is not found:
+if [[ -z $PRINT ]]
+then
+  echo "I could not find that element in the database."
+  exit
+fi
+
+#for each row in the table, extract the element info and make the print: 
+
+echo $PRINT | while IFS="|" read ATOMIC_NUMBER TYPE MASS MELTING_POINT BOILING_POINT SYMBOL NAME
+do
+  echo  "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."
+done
+
+
+
