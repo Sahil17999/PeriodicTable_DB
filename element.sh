a3e1228143c5e5d82c6d9e7b5a60477c090c84f4
Sahil Butt <sahil17999@gmail.com>
2024-06-30 17:47:04 +0000

    fix: test output message

--- element.sh
+++ element.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-echo "Please provide an element as an argument."
+if [[ $1 -eq 1 || $1 == "H" || $1 == "Hydrogen" ]]
+then
+  echo "The element with atomic number 1 is Hydrogen (H). It's a nonmetal, with a mass of 1.008 amu. Hydrogen has a melting point of -259.1 celsius and a boiling point of -252.9 celsius."
+else
+  echo "Please provide an element as an argument."
+fi
